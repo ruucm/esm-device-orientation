6cdd4fb1cbb83cf610b207d260f95a3ab1da3ae7
ruucm <ruucm.a@gmail.com>
2022-04-27 23:10:09 +0900

    fixes

--- src/DeviceOrientation/DeviceOrientation.tsx
+++ src/DeviceOrientation/DeviceOrientation.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDeviceOrientation } from "./useDeviceOrientation";
 
 export const DeviceOrientation = ({
@@ -11,9 +11,19 @@ export const DeviceOrientation = ({
     useDeviceOrientation();
 
   // @ts-ignore
-  useEffect(requestAccess, []);
+  useEffect(() => requestAccess, []);
 
-  if (!orientation) return <div>no access</div>;
+  const onToggle = (toggleState: boolean): void => {
+    const result = toggleState ? requestAccess() : revokeAccess();
+  };
+
+  // need to click once to get the permission at first. (Maybe related to iOS policy)
+  if (!orientation)
+    return (
+      <div>
+        no access <Toggle onToggle={onToggle}>toggle</Toggle>
+      </div>
+    );
 
   const orientationInfo = orientation && (
     <ul>
@@ -37,7 +47,12 @@ export const DeviceOrientation = ({
 
   return React.Children.map(children, (child, id) => (
     <>
-      {React.cloneElement(child, { key: id, variant })}
+      {React.cloneElement(
+        React.isValidElement(child.props.children)
+          ? child.props.children
+          : child,
+        { key: id, variant }
+      )}
       {debug && (
         <pre
           style={{
@@ -48,9 +63,24 @@ export const DeviceOrientation = ({
           }}
         >
           {orientationInfo}
+          variant: {variant}
           {errorElement}
         </pre>
       )}
     </>
   ));
 };
+
+function Toggle({ onToggle }: any) {
+  const [toggleState, setToggleState] = useState(true);
+  return (
+    <button
+      onClick={() => {
+        onToggle(toggleState);
+        setToggleState(!toggleState);
+      }}
+    >
+      {toggleState ? "On" : "Off"}
+    </button>
+  );
+}
